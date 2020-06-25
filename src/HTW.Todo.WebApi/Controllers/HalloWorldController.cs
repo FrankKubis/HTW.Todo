bb22bdb127fdb5d215db898d0931713005e9afe3
Frank Kubis <frankkubis@outlook.de>
2020-06-25 11:09:55 +0200

    Add Hallo World exampel

---
+++ src/HTW.Todo.WebApi/Controllers/HalloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTW.Todo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HalloWorldController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hallo World!");
+        }
+    }
+}
